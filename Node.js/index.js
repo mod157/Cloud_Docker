8020a6c126356a1490890ea2c7925458d8fd090c
DESKTOP-C7J6HNG\mod157 <mod157@naver.com>
2017-12-05 00:01:20 +0900

    host

--- Node.js/index.js
+++ Node.js/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : '129.150.126.241',
   user     : 'Lee',
   password : '32121849',
   port     : 3306,
@@ -12,21 +12,16 @@ var connection = mysql.createConnection({
 
 var app = express();
 
-/*http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type' : 'text/plain' });
-  res.end('Hello World');
-}).listen(8080);*/
-
 app.set('port', process.env.PORT || 8080);
 app.get('/', function(req, res){
+  res.send('Hello World');
   connection.query('SELECT * from info', function(err, rows) {
     if(err) throw err;
 
-    console.log('The solution is: ', rows);
     res.send(rows);
   });
 });
 
 app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+  res.send('Express server listening on port ' + app.get('port'));
 });
