1e942610f5aa6d2cba72d63d530c9fd0ad6c4507
DESKTOP-C7J6HNG\mod157 <mod157@naver.com>
2017-12-04 22:34:54 +0900

    patch

--- Node.js/index.js
+++ Node.js/index.js
@@ -2,6 +2,6 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-        res.writeHead(200, {'Content-Tpye': 'text/html'});
-        res.end(`<h1>${reqconnection.localAddress}</h1>`);
+  res.writeHead(200, { 'Content-Type' : 'text/plain' });
+  res.end('Hello World');
 }).listen(8080);
