00dbf23a7951c4886ccd2aa905977218d26194c2
DESKTOP-C7J6HNG\mod157 <mod157@naver.com>
2017-12-04 22:05:26 +0900

    port

--- Node.js/index.js
+++ Node.js/index.js
@@ -4,4 +4,4 @@ var fs = require('fs');
 http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Tpye': 'text/html'});
         res.end(`<h1>${reqconnection.localAddress}</h1>`);
-}).listen(9999);
+}).listen(8080);
