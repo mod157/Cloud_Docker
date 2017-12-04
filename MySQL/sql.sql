145e499ceb2ece94e6b68b111b91b67a813b1477
DESKTOP-C7J6HNG\mod157 <mod157@naver.com>
2017-12-04 23:22:23 +0900

    express error

---
+++ MySQL/sql.sql
@@ -0,0 +1,11 @@
+CREATE TABLE info
+(
+  id INTEGER AUTO_INCREMENT,
+  student_id INTEGER,
+  name VARCHAR(10),
+  email VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+Insert into info (student_id, name, email)
+values(32121849, 'LeeSunjae', 'Nammu8395@gmail.com');
