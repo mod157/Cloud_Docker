d120a83a2c1b066582f2480334781fa5ef0d0a55
DESKTOP-C7J6HNG\mod157 <mod157@naver.com>
2017-12-04 21:39:36 +0900

    c

--- Node.js/index.js
+++ Node.js/index.js
@@ -1,7 +1,32 @@
 var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var mysql = require('mysql');
 
-http.createServer(function (req, res){
-  res.writeHead(200, {'Content-Type' : 'text/html'});
-  res.end(`<h1>$(req.connection.localAddress}</h1>`);
-}).listen(8080);
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'Lee',
+  password : '32121849',
+  port     : 3306,
+  database : 'Profile'
+});
+
+var app = express();
+
+app.set('port', process.env.PORT || 8080);
+app.get('/', function(req, res){
+  res.send('Root');
+});
+
+app.get('/persons', function(req, res){
+
+  connection.query('SELECT * from Profile', function(err, rows) {
+    if(err) throw err;
+
+    console.log('The solution is: ', rows);
+    res.send(rows);
+  });
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
